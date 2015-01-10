518e83f41c8fb57690b7234d3da10e1a76fb6360
Will Franzen <wtfranzen@gmail.com>
2015-01-10 15:39:41 -0600

    Add bootstrapping code

--- bot.js
+++ bot.js
@@ -1,2 +1,11 @@
 var Reddit  = require('nodewhal');
 var analyze = require('write-good');
+
+var secrets = require('./secrets.json');
+
+var init = function(bot) {
+    console.log(bot);
+};
+
+// Bootstrap
+Reddit('write-good-bot by /u/wtf_are_my_initials').login(secrets.username, secrets.password).then(init);
